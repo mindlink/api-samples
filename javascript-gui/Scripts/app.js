ec29a4c5f61bc7514ba8a07fffc8b58104d3ce36
Ben Osborne <Ben.Osborne@mindlinksoft.com>
2018-10-11 11:26:03 +0100

    Continue to handle CanProvision flag in JS GUI for backwards compatibility

--- javascript-gui/Scripts/app.js
+++ javascript-gui/Scripts/app.js
@@ -121,8 +121,8 @@ var setupBot = function(sipAddress, username, password) {
                 return 'Id: ' + agent.Id + ' state: ' + agent.State; 
             }), '', true);               
         },
-        onProvisionedAgent: function (provisioningMode, channels, id, metaData, state, userName, users) {
-            logMessage('Agent returned. ID: ' + id + ', user name: ' + userName + ', provisioning mode: ' + provisioningMode + ', state: ' + state);
+        onProvisionedAgent: function (provisioningMode, canProvision, channels, id, metaData, state, userName, users) {
+            logMessage('Agent returned. ID: ' + id + ', user name: ' + userName + ', provisioning mode: ' + provisioningMode + ', can provision: ' + canProvision + ', state: ' + state);
             logMessage(' - Channels:')
             logMessage(listAsString(channels, channels.length, function (channel) {
                 return '    - Id: ' + channel.Id + ', state: ' + channel.State; 
